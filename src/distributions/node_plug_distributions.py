f3ae1a927b1b9fc2a7e3a8a496ceb8bc6ec14220
Sanja Scepanovic <sscepano@r415.cern.ch>
2016-11-25 18:01:45 +0100

    this module now also plots distribution of # of jobs on a node and stdevs. need to rename it

--- src/distributions/node_plug_distributions.py
+++ src/distributions/node_plug_distributions.py
@@ -44,15 +44,15 @@ def create_distribution(x):
 
 	d = defaultdict(int)
 	for el in x:
-		d[int(el)] += 1
+		d[int(100*el)] += 1
 	return d
 
 
-def read_in_data_per_node(the_node_type):
+def read_in_data_per_node(the_node_type, the_variable):
 
 	f_in = 'node_plug_n_jobs_variation' + the_node_type + '.csv'
 
-	avg_plug = defaultdict(int)
+	avg_var = defaultdict(float)
 
 
 	with open(f_in, 'r') as f:
@@ -60,47 +60,143 @@ def read_in_data_per_node(the_node_type):
 			node, m1, mm1, avg1, med1, stdev1, var1, \
 			m2, mm2, avg2, med2, stdev2, var2 = line.split()
 
-			avg_plug[node] = float(avg1)
-			if float(avg1) == 0:
-				print node
+			if the_variable == 'avg plug':
+				avg_var[node] = float(avg1)
+				if float(avg1) == 0:
+					print node
+			elif the_variable == 'avg # jobs':
+				avg_var[node] = float(avg2)
+				if float(avg2) == 0:
+					print node
+			elif the_variable == 'plug std':
+				avg_var[node] = float(stdev1)
+				if float(stdev1) == 0:
+					print node
+			elif the_variable == 'plug variance':
+				avg_var[node] = float(var1)
+				if float(var1) == 0:
+					print node
+			elif the_variable == '# jobs std':
+				avg_var[node] = float(stdev2)
+				if float(stdev2) == 0:
+					print node
+			elif the_variable == 'norm std':
+				if float(avg1)== 0:
+					continue
+				avg_var[node] = float(stdev1)/float(avg1)
+				if float(avg_var[node]) < 0.005:
+					print node
 
-	return avg_plug
 
-read_in_data_per_node('SandyBridge')
+	return avg_var
 
-def plot_data(plt,lab='Haswell nodes', xlab='avg plug value', ylab = '# nodes', s=17, \
-	fname = 'distr_of_node_plug.png', col='coral', the_node_type='Haswell'):
+read_in_data_per_node('SandyBridge', 'plug')
 
-	d = create_distribution(read_in_data_per_node(the_node_type).values())
+def plot_data(plt, the_node_type='Haswell', the_variable='plug',s=17,col='coral'):
+
+	lab=the_node_type+' nodes'
+	xlab=the_variable+' value'
+	ylab = '# nodes'
+	fname = the_node_type +'_distr_of_node_' +the_variable+ '.png'
+
+	if the_node_type == 'both':
+		d1 = create_distribution(read_in_data_per_node('Haswell',the_variable).values())
+		d2 = create_distribution(read_in_data_per_node('SandyBridge',the_variable).values())
+	else:
+		d = create_distribution(read_in_data_per_node(the_node_type,the_variable).values())
 
 	ax = fig.add_subplot(111)
-
-	x = np.array(d.keys())
-	y =  np.array(d.values())
-
-	pt_plug = pd.DataFrame({'avg node plug':x})
-	print(pt_plug.describe())
-	
-	ax.scatter(x,y,color=col,s=s,edgecolor='none', label=lab)
-
-
+	if the_node_type == 'both':
+		x1 = np.array(d1.keys())
+		y1 =  np.array(d1.values())
+		pt1 = pd.DataFrame({'node ' + the_variable:x1})
+		print(pt1.describe())
+		#ax.scatter(x1,y1,color='coral',s=s,edgecolor='none', label='Haswell')
+		ax.plot(x1,y1,color='coral',label='Haswell')
+		x2 = np.array(d2.keys())
+		y2 =  np.array(d2.values())
+		pt2 = pd.DataFrame({'node ' + the_variable:x2})
+		print(pt2.describe())
+		#ax.scatter(x2,y2,color='darkblue',s=s,edgecolor='none', label='SandyBridge')
+		ax.plot(x2,y2,color='darkblue', label='SandyBridge')
+	else:
+		x = np.array(d.keys())
+		y =  np.array(d.values())
+		pt_plug = pd.DataFrame({'node ' + the_variable:x})
+		print(pt_plug.describe())
+		#ax.scatter(x,y,color=col,s=s,edgecolor='none', label=lab)
+		ax.plot(x,y,color=col,label=lab)
+		
 	plt.xlabel(xlab)
 	plt.ylabel(ylab)
+	#ax.set_xscale('log')
+	#ax.set_yscale('log')
 	plt.grid(True)
-	#plt.legend(frameon=False, fontsize=14, pos=2)
+
 	handles, labels = ax.get_legend_handles_labels()
-	ax.legend(handles, labels, loc=2)
+	ax.legend(handles, labels, loc=1)
 	plt.grid(True)
 	plt.savefig(fname)
 	print 'Saved in figure ', fname
 
 """
+# plug
 fig = plt.figure()
-plot_data(plt = plt, lab='Haswell nodes', xlab='avg plug value', ylab = '# nodes', s=17, \
-	fname = 'distr_of_node_plug.png', col='coral', the_node_type='Haswell')
-plot_data(plt = plt, lab='SandyBridge nodes', xlab='avg plug value', ylab = '# nodes', s=17, \
-	fname = 'distr_of_node_plug.png', col='darkblue', the_node_type='SandyBridge')
+plot_data(plt, the_node_type='Haswell', the_variable='avg plug',s=17,col='coral')
+fig = plt.figure()
+plot_data(plt, the_node_type='SandyBridge', the_variable='avg plug',s=17,col='darkblue')
+fig = plt.figure()
+plot_data(plt, the_node_type='both', the_variable='avg plug',s=17)
 """
 
+"""
+# plug std
+fig = plt.figure()
+plot_data(plt, the_node_type='Haswell', the_variable='plug std',s=17,col='red')
+fig = plt.figure()
+plot_data(plt, the_node_type='SandyBridge', the_variable='plug std',s=17,col='darkgoldenrod')
+fig = plt.figure()
+plot_data(plt, the_node_type='both', the_variable='plug std',s=17)
+"""
+
+"""
+# plug variance
+fig = plt.figure()
+plot_data(plt, the_node_type='Haswell', the_variable='plug variance',s=17,col='red')
+fig = plt.figure()
+plot_data(plt, the_node_type='SandyBridge', the_variable='plug variance',s=17,col='darkgoldenrod')
+fig = plt.figure()
+plot_data(plt, the_node_type='both', the_variable='plug variance',s=17)
+"""
+
+"""
+# # jobs
+fig = plt.figure()
+plot_data(plt, the_node_type='Haswell', the_variable='avg # jobs',s=17,col='red')
+fig = plt.figure()
+plot_data(plt, the_node_type='SandyBridge', the_variable='avg # jobs',s=17,col='darkgoldenrod')
+fig = plt.figure()
+plot_data(plt, the_node_type='both', the_variable='avg # jobs',s=17)
+"""
+
+"""
+# # jobs std
+fig = plt.figure()
+plot_data(plt, the_node_type='Haswell', the_variable='# jobs std',s=17,col='red')
+fig = plt.figure()
+plot_data(plt, the_node_type='SandyBridge', the_variable='# jobs std',s=17,col='darkgoldenrod')
+fig = plt.figure()
+plot_data(plt, the_node_type='both', the_variable='# jobs std',s=17)
+"""
+
+"""
+# norm std
+fig = plt.figure()
+plot_data(plt, the_node_type='Haswell', the_variable='norm std',s=17,col='red')
+fig = plt.figure()
+plot_data(plt, the_node_type='SandyBridge', the_variable='norm std',s=17,col='darkgoldenrod')
+fig = plt.figure()
+plot_data(plt, the_node_type='both', the_variable='norm std',s=17)
+"""
 
 
